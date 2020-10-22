202b5ac35d1b7682e40a36132c4dadac2aa7c26b
mkalavad <70533438+mkalavad@users.noreply.github.com>
2020-10-22 15:56:56 -0400

    Update script.js

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -36,15 +36,26 @@ document.body.addEventListener('submit', async (e) => {
     .then((fromServer) => fromServer.json())
     .then((fromServer) => {
       // You're going to do your lab work in here. Replace this comment.
-      const country = range(10)
+      const country = range(10);
       const country2 = country.map(()=> {
-        const number = getRandomIntInclusive(0,243)
-        return fromServer[number]
+        const number = getRandomIntInclusive(0,243);
+        return fromServer[number];
 
       })
 
-      const reverse = country2.sort((a,b)=> sortFunction(b,a,"name"))
 
+
+      const reverse = country2.sort((a,b)=> sortFunction(b,a,"name"));
+      const ul = document.createElement('ul');
+      ul.className = 'flex-inner';
+      $('form').prepand(ol);
+
+      reverse.forEach((el, i) => {
+        const li = document.createElement('li');
+        $(li).append(`<input type="checkbox" value=${el.code} id=${el.code} />`);
+        $(li).append(`<label for=${el.code}>${el.name}</label>`);
+        $(ol).append(li);
+      });
       console.log('fromServer', fromServer);
     })
     .catch((err) => console.log(err));
